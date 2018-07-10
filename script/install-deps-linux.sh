1786bf4a3a0ed20573c07ef5e3325763b35548d7
Edward Thomson <ethomson@edwardthomson.com>
2018-07-10 12:40:42 +0100

    ci: update linux deps script

--- script/install-deps-linux.sh
+++ script/install-deps-linux.sh
@@ -2,12 +2,5 @@
 
 set -x
 
-if [ "$MBEDTLS" ]; then
-  git clone --depth 10 --single-branch --branch mbedtls-2.6.1 https://github.com/ARMmbed/mbedtls.git ./deps/mbedtls
-  cd ./deps/mbedtls
-  # We pass -fPIC explicitely because we'll include it in libgit2.so
-  CFLAGS=-fPIC cmake -DENABLE_PROGRAMS=OFF -DENABLE_TESTING=OFF -DUSE_SHARED_MBEDTLS_LIBRARY=OFF -DUSE_STATIC_MBEDTLS_LIBRARY=ON .
-  cmake --build .
-
-  echo "mbedTLS built in `pwd`"
-fi
+apt-get update
+apt-get -y install libssh2-1-dev libcurl4-gnutls-dev openssh-server
