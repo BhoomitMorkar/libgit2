ce926c3ef50cb2134c79fb1abd9420351f5321ae
Edward Thomson <ethomson@edwardthomson.com>
2018-07-10 20:46:36 +0100

    make ssh dir

--- script/install-deps-linux.sh
+++ script/install-deps-linux.sh
@@ -4,3 +4,7 @@ set -x
 
 apt-get update
 apt-get -y install cmake build-essential openssl libssl-dev libssh2-1-dev libcurl4-gnutls-dev openssh-server
+
+if [ ! -d /var/run/sshd ]; then
+	sudo mkdir /var/run/sshd
+fi
