5a1d75e87d34c9c984119c7da3c7b7a86f2fa1cd
Edward Thomson <ethomson@edwardthomson.com>
2018-07-10 20:29:15 +0100

    test

--- script/install-deps-linux.sh
+++ script/install-deps-linux.sh
@@ -3,4 +3,4 @@
 set -x
 
 apt-get update
-apt-get -y install libssh2-1-dev libcurl4-gnutls-dev openssh-server
+apt-get -y install cmake build-essential openssl libssl-dev libssh2-1-dev libcurl4-gnutls-dev openssh-server
