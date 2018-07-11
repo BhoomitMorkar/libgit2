584891881418d25fe1cbcc1a71e1db2756686caa
Edward Thomson <ethomson@edwardthomson.com>
2018-07-11 15:44:17 +0100

    better citest

--- script/citest.sh
+++ script/citest.sh
@@ -48,9 +48,7 @@ fi
 if [ -z "$SKIP_SSH_TESTS" ]; then
 	echo "Starting ssh daemon..."
 	HOME=`mktemp -d ${TMPDIR}/home.XXXXXXXX`
-	SSH_DIR="${HOME}/.ssh"
 	SSHD_DIR=`mktemp -d ${TMPDIR}/sshd.XXXXXXXX`
-	mkdir ${SSH_DIR}
 	cat >"${SSHD_DIR}/sshd_config" <<-EOF
 	Port 2222
 	ListenAddress 0.0.0.0
@@ -68,19 +66,20 @@ if [ -z "$SKIP_SSH_TESTS" ]; then
 	/usr/sbin/sshd -f "${SSHD_DIR}/sshd_config"
 
 	# Set up keys
-	ssh-keygen -t rsa -f "${SSH_DIR}/id_rsa" -N "" -q
-	cat "${SSH_DIR}/id_rsa.pub" >>"${SSH_DIR}/authorized_keys"
+	mkdir "${HOME}/.ssh"
+	ssh-keygen -t rsa -f "${HOME}/.ssh/id_rsa" -N "" -q
+	cat "${HOME}/.ssh/id_rsa.pub" >>"${HOME}/.ssh/authorized_keys"
 	while read algorithm key comment; do
-		echo "[localhost]:2222 $algorithm $key" >>"${SSH_DIR}/known_hosts"
+		echo "[localhost]:2222 $algorithm $key" >>"${HOME}/.ssh/known_hosts"
 	done <"${SSHD_DIR}/id_rsa.pub"
 
 	# Get the fingerprint for localhost and remove the colons so we can
 	# parse it as a hex number. The Mac version is newer so it has a
 	# different output format.
 	if [ "$TRAVIS_OS_NAME" = "osx" ]; then
-		FINGERPRINT=$(ssh-keygen -E md5 -F '[localhost]:2222' -l | tail -n 1 | cut -d ' ' -f 3 | cut -d : -f2- | tr -d :)
+		SSH_FINGERPRINT=$(ssh-keygen -E md5 -F '[localhost]:2222' -f "${HOME}/.ssh/known_hosts" -l | tail -n 1 | cut -d ' ' -f 3 | cut -d : -f2- | tr -d :)
 	else
-		FINGERPRINT=$(ssh-keygen -F '[localhost]:2222' -l | tail -n 1 | cut -d ' ' -f 2 | tr -d ':')
+		SSH_FINGERPRINT=$(ssh-keygen -F '[localhost]:2222' -f "${HOME}/.ssh/known_hosts" -l | tail -n 1 | cut -d ' ' -f 2 | tr -d ':')
 	fi
 fi
 
@@ -139,8 +138,8 @@ if [ -z "$SKIP_SSH_TESTS" ]; then
 
 	export GITTEST_REMOTE_URL="ssh://localhost:2222/$HOME/_temp/test.git"
 	export GITTEST_REMOTE_USER=$USER
-	export GITTEST_REMOTE_SSH_KEY="${SSH_DIR}/id_rsa"
-	export GITTEST_REMOTE_SSH_PUBKEY="${SSH_DIR}/id_rsa.pub"
+	export GITTEST_REMOTE_SSH_KEY="${HOME}/.ssh/id_rsa"
+	export GITTEST_REMOTE_SSH_PUBKEY="${HOME}/.ssh/id_rsa.pub"
 	export GITTEST_REMOTE_SSH_PASSPHRASE=""
 	export GITTEST_REMOTE_SSH_FINGERPRINT="${SSH_FINGERPRINT}"
 	ctest -V -R ssh || die $?
