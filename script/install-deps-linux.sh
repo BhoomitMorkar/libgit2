b6ffd299fbc3d9cde5938969967b7cd3243c9e50
Edward Thomson <ethomson@edwardthomson.com>
2018-07-12 10:44:11 +0100

    huh

--- script/install-deps-linux.sh
+++ script/install-deps-linux.sh
@@ -3,7 +3,7 @@
 set -x
 
 apt-get update
-apt-get -y install cmake build-essential openssl libssl-dev libssh2-1-dev libcurl4-gnutls-dev openssh-server
+apt-get -y install build-essential pkg-config cmake openssl libssl-dev libssh2-1-dev libcurl4-gnutls-dev openssh-server
 
 if [ ! -d /var/run/sshd ]; then
 	sudo mkdir /var/run/sshd
