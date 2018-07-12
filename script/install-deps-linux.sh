fc0706f33354063e06def8bc6f9d54e979143936
Edward Thomson <ethomson@edwardthomson.com>
2018-07-12 10:35:45 +0100

    huh

--- script/install-deps-linux.sh
+++ script/install-deps-linux.sh
@@ -8,3 +8,7 @@ apt-get -y install cmake build-essential openssl libssl-dev libssh2-1-dev libcur
 if [ ! -d /var/run/sshd ]; then
 	sudo mkdir /var/run/sshd
 fi
+
+echo "pkg-config is: $(which pkg-config)"
+echo "libcurl is:    $(pkg-config --modversion libcurl)"
+echo "libssh2 is:    $(pkg-config --modversion libssh2)"
